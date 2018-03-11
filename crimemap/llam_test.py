8fa0afb6ab86e3222f804310fc38069b5cd933b3
youtanhuakai <brant_wu@163.com>
2018-03-11 23:32:31 +0800

    delete files

--- crimemap/llam_test.py
+++
@@ -1,5 +0,0 @@
-import  string
-def sanitize_string(userinput):
-    whilelist = string.ascii_letters + string.digits + "!@#-=_+"
-    return filter(lambda x: x in whilelist, userinput)
-print(list(sanitize_string("aaa")))
